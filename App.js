7c9d3240da618dedf42f053b7929efc6fb1bb942
Diego Rodrigues Vieira <diegorodrigues92@gmail.com>
2020-04-30 22:22:31 -0300

    Adiciona react navigation

--- App.js
+++ App.js
@@ -1,69 +1,28 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from "react";
+import { Text, View } from "react-native";
+import { createBottomTabNavigator } from "react-navigation";
 
-import React, { Component } from "react";
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  NativeModules,
-  DeviceEventEmitter,
-} from "react-native";
-
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu",
-});
-
-const onSdStateChange = (event) => {
-  console.log("state: " + event.sd_state);
-  console.log("reason: " + event.sd_reason);
-  console.log("carrier: " + event.carrier_name);
-  console.log("client ip: " + event.client_ip);
-};
-
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-
-    DeviceEventEmitter.addListener("onSdStateChange", onSdStateChange);
-    NativeModules.SmiSdkReactModule.registerSdStateChangeListner();
-  }
-
+class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Home!</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF",
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10,
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5,
-  },
+class SettingsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Settings!</Text>
+      </View>
+    );
+  }
+}
+
+export default createBottomTabNavigator({
+  Home: HomeScreen,
+  Settings: SettingsScreen,
 });
