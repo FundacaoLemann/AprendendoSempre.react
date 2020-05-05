52fe646ed93f51b1ed827015c7c17ae816aa8976
Diego Rodrigues Vieira <diegorodrigues92@gmail.com>
2020-05-04 23:18:09 -0300

    Add masked view

--- android/app/src/main/java/org/aprendendosempre/app/MainApplication.java
+++ android/app/src/main/java/org/aprendendosempre/app/MainApplication.java
@@ -8,6 +8,7 @@ import com.datami.smisdk_plugin.SmiSdkReactPackage;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.reactnative.maskedview.RNCMaskedViewPackage;
 import com.horcrux.svg.SvgPackage;
 import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -34,6 +35,7 @@ public class MainApplication extends Application implements SdStateChangeListene
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCMaskedViewPackage(),
             new SvgPackage(),
             new SafeAreaContextPackage(),
             new VectorIconsPackage(),
