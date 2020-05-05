af4cef59722921858ab7edd20940a544090d7abe
Diego Rodrigues Vieira <diegorodrigues92@gmail.com>
2020-05-04 22:51:58 -0300

    Add linear gradient

--- android/app/src/main/java/org/aprendendosempre/app/MainApplication.java
+++ android/app/src/main/java/org/aprendendosempre/app/MainApplication.java
@@ -8,6 +8,7 @@ import com.datami.smisdk_plugin.SmiSdkReactPackage;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.swmansion.reanimated.ReanimatedPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements SdStateChangeListene
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LinearGradientPackage(),
             new ReanimatedPackage(),
             new RNGestureHandlerPackage(), new SmiSdkReactPackage()
       );
