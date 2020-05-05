78e9e1dfea336993f7debc64275b25742b07b3aa
Diego Rodrigues Vieira <diegorodrigues92@gmail.com>
2020-05-04 22:37:09 -0300

    Add reanimated

--- android/app/src/main/java/org/aprendendosempre/app/MainApplication.java
+++ android/app/src/main/java/org/aprendendosempre/app/MainApplication.java
@@ -8,6 +8,7 @@ import com.datami.smisdk_plugin.SmiSdkReactPackage;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.reanimated.ReanimatedPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements SdStateChangeListene
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReanimatedPackage(),
             new RNGestureHandlerPackage(), new SmiSdkReactPackage()
       );
     }
