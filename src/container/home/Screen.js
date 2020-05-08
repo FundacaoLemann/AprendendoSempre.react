c61d9386a0867c9b8697bf010a7d0eef085a4a8e
Diego Rodrigues Vieira <diegorodrigues92@gmail.com>
2020-05-08 14:59:45 -0300

    Custom message

--- src/container/home/Screen.js
+++ src/container/home/Screen.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react';
 import {
   ActivityIndicator,
+  Alert,
+  DeviceEventEmitter,
   Dimensions,
   Modal,
   NativeModules,
@@ -45,8 +47,65 @@ const styles = StyleSheet.create({
   },
 });
 
+const data = [
+  {
+    id: 1,
+    title: 'Aprendendo Sempre',
+    image: 'https://aprendendosempre.luby.com.br/imgs/aprendendo_sempre.png',
+    url: 'https://aprendendosempre.org/',
+  },
+  {
+    id: 2,
+    title: 'Aprendizap',
+    image: 'https://aprendendosempre.luby.com.br/imgs/aprendizap-logo.png',
+    url: 'https://www.aprendizap.com.br/',
+  },
+  {
+    id: 3,
+    title: 'Árvore Educação',
+    image: 'https://aprendendosempre.luby.com.br/imgs/arvore_de_livros.png',
+    url: 'https://app.arvoreeducacao.com.br/login?platform=arvore',
+  },
+  {
+    id: 4,
+    title: 'AvaMec',
+    image: 'https://aprendendosempre.luby.com.br/imgs/avamec.png',
+    url: 'http://avamec.mec.gov.br/#/',
+  },
+  {
+    id: 5,
+    title: 'Escola Digital',
+    image: 'https://aprendendosempre.luby.com.br/imgs/escola_digital.png',
+    url: 'https://escoladigital.org.br/',
+  },
+  {
+    id: 6,
+    title: 'Google Sala de Aula',
+    image: 'https://aprendendosempre.luby.com.br/imgs/google_sala_de_aula.png',
+    url: 'https://edu.google.com/intl/pt/products/classroom/?modal_active=none',
+  },
+  {
+    id: 7,
+    title: 'Khan Academy',
+    image: 'https://aprendendosempre.luby.com.br/imgs/khan_academy.png',
+    url: 'https://pt.khanacademy.org/',
+  },
+  {
+    id: 8,
+    title: 'Kinedu',
+    image: 'https://aprendendosempre.luby.com.br/imgs/kinedu.png',
+    url: 'https://www.kinedu.com/pt',
+  },
+  {
+    id: 9,
+    title: 'Nova Escola',
+    image: 'https://aprendendosempre.luby.com.br/imgs/nova_escola.png',
+    url: 'https://novaescola.org.br/',
+  },
+];
+
 function Home() {
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
   const [searchFilter, setSearchFilter] = useState('');
   const [canGoBack, setCanGoBack] = useState(false);
   const [canGoForward, setCanGoForward] = useState(false);
@@ -54,18 +113,52 @@ function Home() {
   const [selectedItem, setSelectedItem] = useState({});
   const insets = useSafeArea();
 
-  useEffect(() => {
-    function fetchData() {
-      fetch('https://aprendendosempre.luby.com.br/json/data.json')
-        .then((response) => response.json())
-        .then((responseData) => setData(responseData))
-        .catch(() => {
-          // no feedback
-        });
-    }
+  const getSponsoredUrl = (url = '') => {
+    return new Promise((resolve) => {
+      NativeModules.SmiSdkReactModule.getSDAuth(
+        'ak-6807dfa3-a5e6-4eb4-8732-e818b9b352c1',
+        url,
+        '',
+        (result) => {
+          if (result && result.url) {
+            resolve(result.url);
+          } else {
+            resolve(url);
+          }
+        },
+      );
+    });
+  };
 
-    fetchData();
+  const onSdStateChange = (event) => {
+    if (event.sd_state === 'SD_AVAILABLE') {
+      Alert.alert('Parabéns!', 'Parabéns! Você está navegando sem consumir seu pacote de dados.');
+    }
+  };
+
+  useEffect(() => {
+    DeviceEventEmitter.addListener('onSdStateChange', onSdStateChange);
+    NativeModules.SmiSdkReactModule.registerSdStateChangeListner();
+
+    return () => {
+      DeviceEventEmitter.removeListener('onSdStateChange');
+    };
   }, []);
+
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const url = await getSponsoredUrl('https://aprendendosempre.luby.com.br/json/data.json');
+
+  //     fetch(url)
+  //       .then((response) => response.json())
+  //       .then((responseData) => setData(responseData))
+  //       .catch(() => {
+  //         // no feedback
+  //       });
+  //   }
+
+  //   fetchData();
+  // }, []);
 
   const {
     spacing,
@@ -82,18 +175,10 @@ function Home() {
     setModalVisible(!modalVisible);
   };
 
-  const handlePressItem = (item) => {
-    NativeModules.SmiSdkReactModule.getSDAuth(
-      'ak-6807dfa3-a5e6-4eb4-8732-e818b9b352c1',
-      item.url,
-      '',
-      (result) => {
-        if (result && result.url) {
-          setSelectedItem({ ...item, url: result.url });
-          toggleModalVisible();
-        }
-      },
-    );
+  const handlePressItem = async (item) => {
+    const url = await getSponsoredUrl(item.url);
+    setSelectedItem({ ...item, url });
+    toggleModalVisible();
   };
 
   const renderContent = () => {
